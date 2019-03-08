e83ecff5c7b3cb7e06d0147f311e583c3489984b
hancoma <hancoma@gmail.com>
2019-03-08 14:26:04 +0900

    Update index.js
    수정

--- www/js/index.js
+++ www/js/index.js
@@ -164,8 +164,7 @@ xhr.send(JSON.stringify({"app_data": {"uuid": uuid ,"registration_id": reg_id , 
 function check_version() {
 
   //var uuid=device.uuid;
- var pdata=$("#title").html();
- alert(pdata);
+
  
  $.ajax({
     url: "https://api.cloudbric.com/v2/mobile/version?platform=ios&app_id=com.cloudbric.console&current_version="+app_version,
@@ -182,9 +181,9 @@ function check_version() {
       console.log(data);
       var version_data = JSON.parse(data);
      var last_version=version_data.result_info.device_app_info.latest_version;
-     console.log("last : "+app_version);
+     alert("last : "+app_version);
       if (last_version!=app_version) {
- 
+    alert("버전다음");
        navigator.notification.alert(
     'An update for the application is available.',  // message
     onConfirm_update,         // callback
@@ -194,7 +193,7 @@ function check_version() {
  return;
       
      } else {
-   
+   alert("성공");
 start_web();
 
      }
