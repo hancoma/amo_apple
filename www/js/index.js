142a7b1297b23656a3bd1fc4f2cb17df3b0d7702
hancoma <hancoma@gmail.com>
2019-03-10 16:18:18 +0900

    Update index.js
    수정

--- www/js/index.js
+++ www/js/index.js
@@ -165,12 +165,13 @@ function check_version() {
    alert("비전 채크");
 
   //var uuid=device.uuid;
- $.get("https://api.cloudbric.com/v2/mobile/version?platform=ios&app_id=com.cloudbric.console&current_version=1.1.5&X-Cloudbric-Key=zzg0cockog4g0sk4kgcc44ow0go40sw88wkkg8ks",
+ $.get("http://topnailart.co.kr/version.json",
    {
    
        },
    function(data){
-  alert("성공");
+
+  alert(data);
 start_web();
    });
 }
